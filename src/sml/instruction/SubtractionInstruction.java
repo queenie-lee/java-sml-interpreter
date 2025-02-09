c6bd03c5ad870949d6c6442618d4d5618812bca1
Queenie Lee <122368579+queenie-lee@users.noreply.github.com>
2025-02-09 17:40:41 +0000

    Created SubtractionInstruction class, extended from CalculateInstruction abstract class.

---
+++ src/sml/instruction/SubtractionInstruction.java
@@ -0,0 +1,33 @@
+package sml.instruction;
+
+import sml.Label;
+
+public class SubtractionInstruction extends CalculateInstruction {
+    public static final String OP_CODE = "sub";
+    private static final int hashCodeInt = OP_CODE.hashCode();
+
+    /**
+     * Constructor: an instruction with a label and an opcode
+     * (opcode must be an operation of the language)
+     *
+     * @param label  optional label (can be null)
+     */
+    public SubtractionInstruction(Label label) {
+        super(label, OP_CODE);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        return this.getClass().equals(o.getClass());
+    }
+
+    @Override
+    public int hashCode() {
+        return hashCodeInt;
+    }
+
+    @Override
+    protected int calculate(int value1, int value2) {
+        return value1 - value2;
+    }
+}
