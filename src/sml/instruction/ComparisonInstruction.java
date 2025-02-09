554f172af3bbf5d14909c9c328ab5a1e779bb85e
Queenie Lee <122368579+queenie-lee@users.noreply.github.com>
2025-02-09 17:57:16 +0000

    Created ComparisonInstruction abstract class, to be used for all comparison subclasses.

---
+++ src/sml/instruction/ComparisonInstruction.java
@@ -0,0 +1,54 @@
+package sml.instruction;
+
+import sml.Frame;
+import sml.Instruction;
+import sml.Label;
+import sml.Machine;
+
+import java.util.Objects;
+import java.util.Optional;
+
+public abstract class ComparisonInstruction extends Instruction  {
+
+    protected final Label branchLabel;
+    /**
+     * Constructor: an instruction with a label and an opcode
+     * (opcode must be an operation of the language)
+     *
+     * @param label  optional label (can be null)
+     * @param opcode operation name
+     */
+    public ComparisonInstruction(Label label, String opcode, Label branchLabel) {
+        super(label, opcode);
+        this.branchLabel = Objects.requireNonNull(branchLabel);
+    }
+
+    /**
+     * Executes the instruction in the given machine.
+     *
+     * @param machine the machine the instruction runs on
+     * @return the new frame with an update instruction index
+     */
+    @Override
+    public Optional<Frame> execute(Machine machine) {
+        Frame frame = machine.frame();
+        int value1 = frame.pop();
+        int value2 = frame.pop();
+        boolean result = compare(value1, value2);
+        return result ? Optional.of(frame.jumpTo(branchLabel)) : Optional.of(frame.advance());
+    }
+
+    protected abstract boolean compare(int value1, int value2);
+
+    /**
+     * Returns a string representation of the operands.
+     * Used in toString().
+     *
+     * @return a string representation of the operands
+     */
+    @Override
+    protected String getOperandsString() {
+        return branchLabel.toString();
+    }
+
+}
