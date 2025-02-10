5319418a696cb461c128b05571f7b8f69d3cb2d4
Queenie Lee <122368579+queenie-lee@users.noreply.github.com>
2025-02-10 21:55:34 +0000

    Created test for overflow and underflow during calculations.

--- test/sml/instruction/CalculateInstructionTest.java
+++ test/sml/instruction/CalculateInstructionTest.java
@@ -93,4 +93,36 @@ class CalculateInstructionTest {
         assertTrue(thrown.getMessage().contains("divide by 0"));
     }
 
+    private static Stream<Arguments> argumentsForOverflowAndUnderflowTest() {
+        return Stream.of(
+                Arguments.of(new AdditionInstruction(null), Integer.MAX_VALUE, 10),
+                Arguments.of(new AdditionInstruction(null), Integer.MIN_VALUE, -10),
+                Arguments.of(new SubtractionInstruction(null), -10, Integer.MAX_VALUE),
+                Arguments.of(new SubtractionInstruction(null), 10, Integer.MIN_VALUE),
+                Arguments.of(new MultiplicationInstruction(null), Integer.MAX_VALUE, 10),
+                Arguments.of(new MultiplicationInstruction(null), Integer.MAX_VALUE, -10)
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("argumentsForOverflowAndUnderflowTest")
+    void calculationResultIsAnOverflowOrUnderflowNumber (Instruction ins0, int value1, int value2) {
+        Instruction ins1 = new PrintInstruction(null);
+        Method m = new Method(new Method.Identifier("@main"),
+                List.of(), List.of(ins0, ins1));
+        machine.setProgram(List.of(m));
+
+        machine.frame().push(value2);
+        machine.frame().push(value1); // flipped order so that value1 is at the top of the stack
+
+        BadProgramError thrown = assertThrows(
+                BadProgramError.class,
+                () -> ins0.execute(machine),
+                "Expected execute(machine) to throw, but it didn't"
+        );
+
+        assertTrue(thrown.getMessage().contains("overflow"));
+
+    }
+
+
 }
