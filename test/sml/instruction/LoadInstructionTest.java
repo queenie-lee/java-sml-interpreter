25373c63d5fe574e205f4552ab7547a73ef54e7f
Queenie Lee <122368579+queenie-lee@users.noreply.github.com>
2025-02-12 23:34:10 +0000

    Created tests for LoadInstruction.

---
+++ test/sml/instruction/LoadInstructionTest.java
@@ -0,0 +1,65 @@
+package sml.instruction;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import sml.*;
+
+import java.io.PrintStream;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LoadInstructionTest {
+
+    private Machine machine;
+
+    @BeforeEach
+    void setUp() {
+        machine = new Machine();
+    }
+
+    @AfterEach
+    void tearDown() {
+        machine = null;
+    }
+
+    @Test
+    void loadInstructionSuccessful() throws BadProgramError {
+        Variable.Identifier variable = new Variable.Identifier("Larry");
+        Instruction ins0 = new LoadInstruction(null, "Larry");
+        Instruction ins1 = new ReturnInstruction(null);
+
+        Method m = new Method(new Method.Identifier("@main"),
+                List.of(), List.of(ins0, ins1));
+        machine.setProgram(List.of(m));
+        machine.frame().variable(variable).store(77);
+
+        ins0.execute(machine);
+        int result = machine.frame().pop();
+
+        assertEquals(77, result);
+    }
+
+    @Test
+    void loadingVariableWithoutValue() throws BadProgramError {
+        Variable.Identifier variable = new Variable.Identifier("Pete");
+
+        Instruction ins0 = new LoadInstruction(null, variable.name());
+        Instruction ins1 = new ReturnInstruction(null);
+
+        Method m = new Method(new Method.Identifier("@main"),
+                List.of(), List.of(ins0, ins1));
+        machine.setProgram(List.of(m));
+        BadProgramError ex = assertThrows(BadProgramError.class, () -> ins0.execute(machine));
+
+        assertEquals("The variable " + variable.name() + " does not contain a value.", ex.getMessage());
+////        // TODO: improve the test by adding fields for the label and method in the exception class
+    }
+
+}
